0feb05992abdc3831b35fda1ceee32dc02fd56e5
EduardoGarrido90 <ecgarrido@comillas.edu>
2024-10-17 22:43:40 +0200

    refactorizing

--- demo/main.py
+++ demo/main.py
@@ -12,26 +12,6 @@ def make_env(max_actions):
         return env
     return _init  # Retorna la función _init, no el entorno directamente
 
-from typing import Callable
-def linear_schedule(initial_value: float) -> Callable[[float], float]:
-    """
-    Linear learning rate schedule.
-
-    :param initial_value: Initial learning rate.
-    :return: schedule that computes
-      current learning rate depending on remaining progress
-    """
-    def func(progress_remaining: float) -> float:
-        """
-        Progress will decrease from 1 (beginning) to 0.
-
-        :param progress_remaining:
-        :return: current learning rate
-        """
-        return progress_remaining * initial_value
-
-    return func
-
 import matplotlib
 import matplotlib.pyplot as plt
 from stable_baselines3.common.vec_env import SubprocVecEnv
