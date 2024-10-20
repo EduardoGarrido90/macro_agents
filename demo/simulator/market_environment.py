f49f4ddc9184a77dd5d4ab19f072c4e6d0c82704
EduardoGarrido90 <ecgarrido@comillas.edu>
2024-10-20 19:47:13 +0200

    More content on the simulator

--- demo/simulator/market_environment.py
+++ demo/simulator/market_environment.py
@@ -74,23 +74,26 @@ class MarketEnv(gymnasium.Env):
 
         # Demand function: assume a linear demand curve (demand decreases as price increases)
         base_demand = self.production_limit_per_producer * 3.5  # Maximum demand when price is 0
-        self.total_demand = max(0, base_demand - 3 * self.price) #TODO: La demanda deberia ser cuadratica, y ademas es baja.
+        elasticity = 1.5  # Aumentar este valor hace que la demanda sea más sensible a los cambios de precio
+        self.total_demand = max(0, base_demand - (self.price ** elasticity))
+        #self.total_demand = max(0, base_demand - 3 * self.price) #TODO: La demanda deberia ser cuadratica, y ademas es baja.
 
         # Cubic production cost for the agent, falta el logaritmo en base 1.1 para hacerla mas plana.
         production_cost = (self.cost_coefficients[3] * (producer_quantity ** 3) + self.cost_coefficients[2] * (producer_quantity ** 2) + self.cost_coefficients[1] * producer_quantity)*8.0
         production_cost = np.random.normal(loc=production_cost, scale=production_cost*PRODUCTION_NOISE)
 
+        if producer_quantity > self.total_demand:
+            excess_units = producer_quantity - self.total_demand
+            storage_penalty = excess_units ** 2 * storage_cost_factor  # Penalización cuadrática por exceso
+            production_cost += storage_penalty
 
         # Adjust price based on the market-clearing condition and production cost
-        #TODO: El precio debe variar mas abruptamente en base al resto, cambia muy poco.
-        #Le damos al agente muy poco margen de maniobra para reaccionar.
-        #La subida del precio debe no ser fija.
         if self.total_demand > self.total_supply:
-            self.price = min(self.price + 1, 200)  # Price cap at 200
-            #self.price = min(self.price + 1, 100, production_cost+1)  # Price cap at 100
+            price_adjustment = (self.total_demand - self.total_supply) / (self.total_supply + 1)
+            self.price = min(self.price + price_adjustment, 200) # Price cap at 200
         else:
-            #self.price = max(self.price - 1, production_cost+1)  # Minimum price of 1
-            self.price = max(self.price - 1, 1)  # Minimum price of 1
+            price_adjustment = (self.total_supply - self.total_demand) / (self.total_demand + 1)
+            self.price = max(self.price - price_adjustment, 1)
 
 
         # Producer's revenue
