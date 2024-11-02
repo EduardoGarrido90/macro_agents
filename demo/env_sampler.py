805146efa9e64c33aea6db0f76b098b95f9a350c
EduardoGarrido90 <ecgarrido@comillas.edu>
2024-11-02 10:15:36 +0100

    WIP. Sampling environments

--- demo/env_sampler.py
+++ demo/env_sampler.py
@@ -1,10 +1,17 @@
 from config.config_parser import ConfigParser
-
+import numpy as np
 # Example usage
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    n_samples = 25
+
     config_parser = ConfigParser('./config/config_scenario.json')
     parameters = config_parser.get_parameters()
     print(parameters)
+    environments = []
+    for i in range(n_samples):
+        environments.append(config_parser.sample_configuration())
+    print(environments)
 
-    sampled_config = config_parser.sample_configuration()
-    print(sampled_config)
+    #TODO: Train all the agents in the 25 samples.
+    #TODO: Recopilate the result of the agents.
+    #TODO: Perform statistical hypothesis testing.
